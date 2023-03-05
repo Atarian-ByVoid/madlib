181680819b3dc6c74f3e956a77f369f5bbf7665f
Carlos Alberto <cgomespimentel7@gmail.com>
2023-03-05 20:41:10 -0300

    New Madlib

---
+++ madlib.py
@@ -0,0 +1,37 @@
+nome = input("Nome: ")
+adj1 = input("Adjetivo: ")
+adj2 = input("Adjetivo: ")
+adj3 = input("Adjetivo: ")
+adj4 = input("Adjetivo: ")
+calcado = input("Calçado: ")
+obj1 = input("Objeto: ")
+obj2 = input("Objeto: ")
+animal = input ("Animal: ")
+lugar = input ("Lugar: ")
+verbgerundio = input("Verbo no gerundio: ")
+verbgerundio2 = input("Verbo no gerundio: ")
+verbinfini = input("Verbo no infinitivo: ")
+verbinfini2 = input("Verbo no infinitivo: ")
+adjespecial = input ("Adjetivo especial: ")
+objespecial = input ("Objeto especial: ")
+objinterno = input ("Objeto: ")
+objetosupresa = input ("Objeto supresa: ")
+emocao = input ("Emoção: ")
+emocao2 = input ("Emoção: ")
+
+
+
+
+madlib = f"""
+Um dia, {nome} acordou se sentindo {adj1} e decidiu sair em uma aventura. Ele/Ela vestiu uma roupa {adj2}, calçou seus/suas {calcado}, pegou sua {obj1} e saiu pela porta.
+
+No caminho, {nome} encontrou um {animal} {adj3} que estava {verbgerundio}. Ele/Ela decidiu ajudar o {animal} e juntos, eles/elas foram em direção à {lugar}.
+
+No meio do caminho, eles/elas encontraram um {obj2} {adj4} e decidiram usá-lo para {verbinfini}. Depois de {verbgerundio2}, eles/elas finalmente chegaram ao {lugar}, onde encontraram um {adjespecial} {objespecial} que continha {objinterno} dentro.
+
+{nome} e o {animal} ficaram surpresos/ surpresas ao verem o {adjespecial} {objespecial} e decidiram {verbinfini2}. Quando eles/elas abriram o {objespecial}, eles/elas encontraram {objetosupresa} dentro e ficaram {emocao}.
+
+Depois dessa aventura maluca, {nome} decidiu que era hora de voltar para casa. Ele/Ela se despediu do {animal}, guardou o {objetosupresa} em sua mochila e voltou para casa, sentindo-se {emocao2}.
+
+"""
+print (madlib)
